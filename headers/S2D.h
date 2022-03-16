5c56d2a89eeb3278cab1507fbe4b19ca153fc2b9
Ren <96073233+Renniren@users.noreply.github.com>
2022-03-16 15:18:32 -0400

    function pointer madness

--- headers/S2D.h
+++ headers/S2D.h
@@ -16,44 +16,142 @@ using namespace std;
 #ifndef S2D_SETTINGS
 #define S2D_SETTINGS
 
-string game_name = "S2D Empty - ";
+string game_name = "S2D Game - ";
+
+#endif
+
+#ifndef S2D_EVENTS
+#define S2D_EVENTS
+
+template <typename type>
+class Event
+{
+	type returnType;
+	type fn;
+	vector<type> events;
+public:
+	Event(void* func)
+	{
+		type (*function)(type){ func };
+		fn = function;
+	}
+
+	Event(vector<void*> events)
+	{
+		for (int i = 0; i < events.size(); int i++)
+		{
+			type(*function)(type) { events[i] };
+			this->events.push_back(function);
+		}
+	}
+
+	void Subscribe(void* method)
+	{
+		type(*function)(type) { method };
+		events.push_back(function);
+	}
+
+	void Invoke()
+	{
+		(*fn)();
+	}
+};
 
 #endif
 
 RenderWindow* GAME_WINDOW;
 bool doClear = true;
+class S2D_event_handler
+{
+public:
+	__event void game_tick();
+
+};
+
+S2D_event_handler* evh;
 
 class world
 {
 public:
 	string name;
 	Color clear_color;
-
 };
+
+enum simulate_flags { SimulateOnlyWhenLevelActive, SimulateAlways, SimulateAlwaysDontDraw };
 
 class level
 {
 public:
 	string name;
 	world world_settings;
-	class world_object
-	{
-		Vector2<float> position, scale;
-		level* current_level;
-
-		world_object();
-
-		void levelChanged()
-		{
-
-		}
-	};
 };
 
-world default_world = { "Default World", Color::Black };
+level* current_level;
+
+class world_object
+{
+public:
+	Vector2f position, scale;
+	level* parent_level;
+	Sprite sprite;
+	simulate_flags flags = simulate_flags::SimulateOnlyWhenLevelActive;
+	string name;
+	float rotation;
+
+	void levelChanged()
+	{
+
+	}
+
+	void tick()
+	{
+		printf("t");
+		switch (flags)
+		{
+		case SimulateAlways:
+			sprite.setPosition(position);
+			sprite.setRotation(rotation);
+			sprite.setScale(scale);
+			GAME_WINDOW->draw(sprite);
+			break;
+
+		case SimulateAlwaysDontDraw:
+
+			break;
+
+		case SimulateOnlyWhenLevelActive:
+			if (parent_level != current_level) return;
+				sprite.setPosition(position);
+				sprite.setRotation(rotation);
+				sprite.setScale(scale);
+				GAME_WINDOW->draw(sprite);
+			break;
+		}
+	}
+
+	world_object()
+	{
+		parent_level = nullptr;
+		rotation = 0;
+		position = Vector2f(0, 0);
+		name = "new WorldObject";
+		//__hook(&S2D_event_handler::game_tick, evh, &world_object::tick);
+		printf("\nd");
+	}
+};
+
+class player
+{
+public:
+	void update()
+	{
+		printf("\ng");
+	}
+};
+
+world default_world = { "No World", Color::Black };
 world current_world = default_world;
 
-level* current_level;
 
 void set_current_level(level* destination)
 {
@@ -71,7 +169,23 @@ void set_current_level(level* destination)
 	}
 }
 
-//keeps record of every loaded texture, and their accompanying sprite
+//keeps record of every loaded texture, and their accompanying sprite to keep textures alive
 vector<pair<Texture*, Sprite*>> textures;
+void create_texture_pair(Texture* tex, Sprite* spr)
+{
+	textures.push_back(pair<Texture*, Sprite*>(tex, spr));
+}
+
+Sprite* create_sprite(string texturepath)
+{
+	Texture* texture = new Texture();
+	Sprite* ret;
+	if(!texture->loadFromFile(texturepath)) return nullptr;
+	ret = new Sprite(*texture);
+	create_texture_pair(texture, ret);
+	ret->setOrigin(0.25f, 0.25f);
+	return ret;
+}
+
 
 #endif
--- main.cpp
+++ main.cpp
@@ -3,11 +3,16 @@
 int main()
 {
   
-  RenderWindow window(VideoMode(800, 600), "window");
-  GAME_WINDOW = &window;
-  printf("Hello world!");
+    RenderWindow window(VideoMode(800, 600), "window");
+    GAME_WINDOW = &window;
+    
+    evh = new S2D_event_handler();
+    
+    printf("Hello world!");
   
     sf::Event event;
+    player* test = new player();
+
     while(GAME_WINDOW->isOpen())
     {
         while (GAME_WINDOW->pollEvent(event))
@@ -15,6 +20,9 @@ int main()
             if (event.type == sf::Event::Closed)
                 GAME_WINDOW->close();
         }
+
+        __raise evh->game_tick();
+
         GAME_WINDOW->setTitle(game_name + current_world.name);
         if(doClear) GAME_WINDOW->clear(current_world.clear_color);
         GAME_WINDOW->display();
