5b51106b9b2593a23d0a43c82a3cea8595ac993e
Ren <96073233+Renniren@users.noreply.github.com>
2022-10-31 15:54:21 -0400

    Beginning work on serialization/deserialization

--- headers/S2Ddata.h
+++ headers/S2Ddata.h
@@ -2,14 +2,38 @@
 #ifndef S2D_DATA_HEADER
 #define S2D_DATA_HEADER
 #include <S2Dcomponents.h>
+
 #ifndef S2D_DATA_MACROS
 #define S2D_DATA_MACROS
 
 #endif
 
-class LevelFileParser
+class LevelSerializer
 {
+#define cstr const char*
+public:
+	cstr FILE_BEGIN = "FILE:";
+	cstr FILE_END = "END";
 
+	cstr SCOPE_BEGIN = "{";
+	cstr SCOPE_END = "}";
+	
+	cstr ARR_BEGIN = "[";
+	cstr ARR_END = "]";
+
+	cstr ATTR_BEGIN = "(";
+	cstr ATTR_END = ")";
+	
+	std::string SerializeLevel(Level* lvl)
+	{
+		using namespace std;
+		string result = "";
+		result.append(FILE_BEGIN);
+
+		result.append(FILE_END);
+	}
+
+#undef cstr
 };
 
 #endif
